9d429e8342c06b26cfc3bc72db5e1b60b514dc25
RandelRussell <u201910669@upc.edu.pe>
2024-04-24 20:08:25 -0500

    feat(in-app-navigation): added in-app navigation initial routes

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,47 @@
-<script setup>
+<script>
+export default {
+  name: "app",
+  title: "ACME Learning Center",
 
+  data() {
+    return {
+      drawer: false,
+      items: [
+        {label: 'Home', to: '/home'},
+        {label: 'About', to: '/about'},
+      ]
+    }
+  },
+  methods: {
+    toggleDrawer() {
+      this.drawer = !this.drawer;
+    },
+
+  }
+}
 </script>
+
 <template>
+  <pv-toast></pv-toast>
+  <header>
+    <pv-toolbar class="bg-primary" fixed>
+      <template #start>
+        <pv-button class="p-button-text text-white" icon="pi pi-bars" @click="toggleDrawer()"></pv-button>
+        <h3>ACME Learning Center</h3>
 
+        <div class="flex-column">
+          <router-link v-for="item in items" :key="item.label" v-slot="{ navigate, href}" :to="item.to" custom>
+            <pv-button :href="href" class="p-button-text text-white" @click="navigate">
+              {{ item.label }}
+            </pv-button>
+          </router-link>
+        </div>
+      </template>
+    </pv-toolbar>
+  </header>
+  <pv-sidebar v-model:visible="drawer"></pv-sidebar>
+  <router-view></router-view>
 </template>
-<style scoped>
 
+<style scoped>
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import {createRouter, createWebHistory} from "vue-router";
+import HomeComponent from "../public/pages/home.component.vue";
+import AboutComponent from "../public/pages/about.component.vue";
+import PageNotFoundComponent from "../public/pages/page-not-found.component.vue";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/home', component: HomeComponent, meta: { title: 'Home' }, },
+        { path: '/about', component: AboutComponent, meta: { title: 'About us' }, },
+        { path: '/:pathMatch(.*)*', component: PageNotFoundComponent },
+        { path: '/', redirect: '/home' }
+    ]
+});
+
+router.beforeEach((to, from, next) => {
+    let baseTitle = 'ACME Learning Center';
+    document.title = `${ baseTitle } | ${to.meta["title"]}`;
+    next();
+});
+export default router;
